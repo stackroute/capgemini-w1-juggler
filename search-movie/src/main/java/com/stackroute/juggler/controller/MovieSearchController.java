376c624bcfea1fb1bdc84d2db02cdd4c566288ea
Anmisha <anmisha99@gmail.com>
2018-08-21 18:25:00 +0530

    search microservice

---
+++ search-movie/src/main/java/com/stackroute/juggler/controller/MovieSearchController.java
@@ -0,0 +1,5 @@
+//package com.stackroute.juggler.controller;
+//
+//public class MovieSearchController {
+//
+//}
---
+++ search-movie/src/main/java/com/stackroute/juggler/domain/City.java
@@ -0,0 +1,20 @@
+package com.stackroute.juggler.domain;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//pojo for city
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+
+		@Id
+		private String city;
+		private List<Movie> movieList;
+	}
+
