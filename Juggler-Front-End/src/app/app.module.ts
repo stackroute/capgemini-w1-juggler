01fb7ab22df3ec3626e9a8ca52d708341747366f
Anmisha <anmisha99@gmail.com>
2018-08-22 18:14:33 +0530

    movie-detail

--- Juggler-Front-End/src/app/app.module.ts
+++ Juggler-Front-End/src/app/app.module.ts
@@ -9,16 +9,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { MoviedetailsFormComponent } from './moviedetails-form/moviedetails-form.component';
 
-
+// import { MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     routingComponents,
-    UserLoginComponent
+    UserLoginComponent,
+    MoviedetailsFormComponent
+    
   ],
+  
   imports: [
     BrowserModule,
     FormsModule,
