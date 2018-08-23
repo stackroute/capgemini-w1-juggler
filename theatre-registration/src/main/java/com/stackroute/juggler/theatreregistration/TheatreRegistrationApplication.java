9a2d7e11ad03c3723415e7afbcc39fcc9c45250e
leela maithreyi <mythrineduri@gmail.com>
2018-08-23 20:29:55 +0530

    kafka included

--- theatre-registration/src/main/java/com/stackroute/juggler/theatreregistration/TheatreRegistrationApplication.java
+++ theatre-registration/src/main/java/com/stackroute/juggler/theatreregistration/TheatreRegistrationApplication.java
@@ -2,12 +2,14 @@ package com.stackroute.juggler.theatreregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TheatreRegistrationApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context=SpringApplication.run(TheatreRegistrationApplication.class, args);
+		SpringApplication.run(TheatreRegistrationApplication.class, args);
 	}
 }
