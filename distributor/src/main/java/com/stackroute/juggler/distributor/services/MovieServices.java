290a766a0834d74664c1b152a7364f270ddb9215
Bajjuri Sai Teja <bsaiteja1996@gmail.com>
2018-08-23 10:33:32 +0530

    distributorservice

--- distributor/src/main/java/com/stackroute/juggler/distributor/services/MovieServices.java
+++ distributor/src/main/java/com/stackroute/juggler/distributor/services/MovieServices.java
@@ -1,10 +1,13 @@
-//package com.stackroute.juggler.distributor.services;
-//
-//import com.stackroute.juggler.distributor.domain.Movie;
-//
-//public interface MovieServices {
-//
-//	public Movie saveMovie(Movie movie);
-//	
-//	
-//}
+package com.stackroute.juggler.distributor.services;
+
+import java.util.List;
+
+import com.stackroute.juggler.distributor.domain.Movie;
+public interface MovieServices {
+
+	public Movie saveMovie(Movie movie);
+	public List<Movie> getByMovieTitle(String movieTitle);
+	public List<Movie> getAllMovies();
+//	public Movie updateMovie(Movie movie);
+	
+}
