3889a559bc5c0720f18c2836e48143e4e08c45c2
Sai Saketh Maringanti <saketh090597@gmail.com>
2018-08-28 16:51:51 +0530

    issues edited

--- distributor/src/main/java/com/stackroute/juggler/distributor/controller/MovieController.java
+++ distributor/src/main/java/com/stackroute/juggler/distributor/controller/MovieController.java
@@ -2,19 +2,20 @@ package com.stackroute.juggler.distributor.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.stackroute.juggler.distributor.domain.Movie;
-import com.stackroute.juggler.distributor.services.MovieServices;
-import com.stackroute.juggler.distributor.services.MovieServicesImpl;
+import com.stackroute.juggler.distributor.services.Services;
 
 @CrossOrigin(origins = "http://localhost:4200")
 //This is controller
