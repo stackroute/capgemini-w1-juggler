109f52bd8a839957296f1c758172be019df091c6
Anmisha <anmisha99@gmail.com>
2018-09-10 11:03:28 +0530

    changes done

---
+++ promocodes/src/main/java/com/stackroute/juggler/promocodes/PromocodesApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.juggler.promocodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocodesApplication.class, args);
+	}
+}
