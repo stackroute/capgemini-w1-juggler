b777ef1204e987fd05725cf587434e002570caff
Saishree <saishreevalluripally@gmail.com>
2018-09-12 15:02:37 +0530

    Rsvp microservice

---
+++ rsvp/src/main/java/com/stackroute/juggler/rsvp/repository/EventRepository.java
@@ -0,0 +1,14 @@
+package com.stackroute.juggler.rsvp.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.stackroute.juggler.rsvp.domain.EventDetails;
+//This repository is used to perform various operations on database	
+@Repository
+public interface EventRepository extends MongoRepository < EventDetails, String>{
+
+	public EventDetails getByEmailId(String emailId);
+	
+	public EventDetails getByEventName(String eventName);
+}
