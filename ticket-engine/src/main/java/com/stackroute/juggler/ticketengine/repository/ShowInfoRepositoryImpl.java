acbbff3cfbcadbb2855c2a47565d5c2d48ba024c
Vishnu <mvvishnu369@gmail.com>
2018-09-15 11:42:06 +0530

    rsvp

--- ticket-engine/src/main/java/com/stackroute/juggler/ticketengine/repository/ShowInfoRepositoryImpl.java
+++ ticket-engine/src/main/java/com/stackroute/juggler/ticketengine/repository/ShowInfoRepositoryImpl.java
@@ -4,16 +4,18 @@ import java.util.Map;
 
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.stackroute.juggler.ticketengine.domain.Show;
 
+@Repository
 public class ShowInfoRepositoryImpl implements ShowInfoRepository{
 
-	private RedisTemplate<String, Show> redisTemplate;
+	private RedisTemplate<String, Object> redisTemplate;
 	
 	private HashOperations<String, String, Show> hashOperations;
 	
-	public ShowInfoRepositoryImpl(RedisTemplate<String, Show> redisTemplate) {
+	public ShowInfoRepositoryImpl(RedisTemplate<String, Object> redisTemplate) {
 		this.redisTemplate = redisTemplate;
 		hashOperations = redisTemplate.opsForHash();
 	}
---
+++ ticket-engine/src/main/java/com/stackroute/juggler/ticketengine/service/SeatsService.java
@@ -0,0 +1,22 @@
+package com.stackroute.juggler.ticketengine.service;
+
+import java.util.Optional;
+
+import com.stackroute.juggler.ticketengine.domain.Seats;
+
+public interface SeatsService {
+	
+	public Seats save(Seats seats);
+	
+	public Seats update(Seats seats);
+	
+	public Optional<Seats> getById(String id);
+	
+	public void delete(String id);
+
+	public Iterable<Seats> getAll();
+	
+	public void removeAll();
+	
+}
+
