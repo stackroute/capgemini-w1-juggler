43f6e315f4cbddd42c65cc4dc3a60f2e3f9900cb
Anmisha <anmisha99@gmail.com>
2018-09-17 10:18:51 +0530

    modified .gitignore and updated procode service

--- promocodes/src/main/java/com/stackroute/juggler/promocodes/config/CommandLineAppStartupRunner.java
+++ promocodes/src/main/java/com/stackroute/juggler/promocodes/config/CommandLineAppStartupRunner.java
@@ -30,11 +30,11 @@ import com.stackroute.juggler.promocodes.repository.PromocodeRepository;
 			Promocodes promo2=new Promocodes(2, "FLAT200","http://4.bp.blogspot.com/-A3aLFetzU34/VhVklMdjOcI/AAAAAAAAASs/xPSWi_SyPuk/s1600/Untitled1865-358x256.png" , "It is applicable from 1000Rs on ticket", 200);
 			Promocodes promo3=new Promocodes(3, "FLAT50","http://4.bp.blogspot.com/-A3aLFetzU34/VhVklMdjOcI/AAAAAAAAASs/xPSWi_SyPuk/s1600/Untitled1865-358x256.png", "It is applicable from 300Rs on ticket", 50);
 			Promocodes promo4=new Promocodes(4, "FLAT20", "http://4.bp.blogspot.com/-A3aLFetzU34/VhVklMdjOcI/AAAAAAAAASs/xPSWi_SyPuk/s1600/Untitled1865-358x256.png","It is applicable from 200Rs on ticket", 20);
-			Promocodes promo5=new Promocodes(5, "FLAT10", "http://4.bp.blogspot.com/-A3aLFetzU34/VhVklMdjOcI/AAAAAAAAASs/xPSWi_SyPuk/s1600/Untitled1865-358x256.png", "It is applicable from 100Rs on ticket", 10);
+//			Promocodes promo5=new Promocodes(5, "FLAT10", "http://4.bp.blogspot.com/-A3aLFetzU34/VhVklMdjOcI/AAAAAAAAASs/xPSWi_SyPuk/s1600/Untitled1865-358x256.png", "It is applicable from 100Rs on ticket", 10);
 			promoRepository.save(promo1);
 			promoRepository.save(promo2);
 			promoRepository.save(promo3);
 			promoRepository.save(promo4);
-			promoRepository.save(promo5);
+//			promoRepository.save(promo5);
 		}
 }
