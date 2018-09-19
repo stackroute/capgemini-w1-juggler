6dead09030b8036414b9c7906a6e28555bbbd0f5
jyothirmayee <jyothirmayee.24.6.1997@gmail.com>
2018-09-19 12:35:30 +0530

    added seat layout domain

--- Juggler-Front-End/src/app/search-data.service.ts
+++ Juggler-Front-End/src/app/search-data.service.ts
@@ -8,8 +8,8 @@ import { map } from "rxjs/operators";
 })
 export class SearchDataService {
   data: any;
-  private url3="http://172.23.239.112:8060/api/v1/city";
-  private url = "http://172.23.239.112:8060/api/v1/movie";
+  private url3="http://10.20.1.15:9077/api/v1/city";
+  private url = "http://10.20.1.15:9077/api/v1/movie";
 
   constructor(private http: HttpClient, private router: Router) {}
 
