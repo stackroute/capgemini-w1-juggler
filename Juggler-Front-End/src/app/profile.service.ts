8ac057f9d44f6a5d531fee104b39891ef70cb925
jyothirmayee <jyothirmayee.24.6.1997@gmail.com>
2018-09-19 13:25:20 +0530

    changed urls

--- Juggler-Front-End/src/app/profile.service.ts
+++ Juggler-Front-End/src/app/profile.service.ts
@@ -5,6 +5,6 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProfileService {
-  private _url = 'http://172.23.239.112:8020';
+  private _url = 'http://10.20.1.15:9073';
   constructor(private http: HttpClient) {}
 }
